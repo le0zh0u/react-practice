60d2bac6c2b9c69c5261bead25aa4889af78e0fd
le0zh0u <492753662@qq.com>
2016-11-17 17:10:26 +0800

    add counter project && init npm and express serverand react dependencies

---
+++ counter/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry:[
+    'webpack-hot-middleware/client',
+    './index'
+  ],
+  output:{
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static'
+  },
+  plugins: [
+    //OccurenceOrderPlugin 给经常使用的模块分配最小长度的id
+    new webpack.optimize.OccurenceOrderPlugin(),
+    //HotModuleReplacementPlugin 热替换，不用刷新页面
+    new webpack.HotModuleReplacementPlugin(),
+    //NoErrorsPlugin 保证编译后的代码永远是对的
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test:/\.js$/,
+        loaders:['babel'],
+        exclude:/node_modules/,
+        include:__dirname
+      }
+    ]
+  }
+}
